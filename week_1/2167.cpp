0c1231aad9134babe224cf498aa58b9746006f4f
1876070677 <fixerpremium@gmail.com>
2024-01-03 14:39:28 +0900

    Add: week_1 folder

---
+++ week_1/2167.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int arr[300][300];
+	int N, M;
+	cin >> N >> M;
+	int c, d;
+	for (c = 0; c < N; c++) {
+		for (d = 0; d < M; d++) {
+			int input;
+			cin >> input;
+			int l = 0, u = 0, sq = 0;
+			if (c - 1 > -1)
+				u = arr[c-1][d];
+			if (d - 1 > -1)
+				l = arr[c][d-1];
+			if (c - 1 > -1 && d - 1 > -1)
+				sq = arr[c-1][d-1];
+			arr[c][d] = u + l - sq + input;
+		}
+	}
+
+	// for (c = 0; c < N; c++) {
+	// 	for (d = 0; d < M; d++) {
+	// 		cout << arr[c][d] << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	int K;
+	cin >> K;
+	for (c = 0; c < K; c++) {
+		int i, j, x, y;
+		cin >> i >> j >> x >> y;
+		int l = 0, u = 0, sq = 0;
+		if (j - 2 > -1)
+			l = arr[x - 1][j - 2];
+		if (i - 2 > -1)
+			u = arr[i - 2][y - 1];
+		if (i - 2 > -1 && j - 2 > -1)
+			sq = arr[i - 2][j - 2];
+		cout << arr[x - 1][y - 1] - (l + u - sq) << endl;
+	}
+}
---
+++ week_1/2669.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int arr[100][100];
+	fill(arr[0], arr[100], 0);
+	int i;
+	int maxX = -1, maxY = -1;
+	int sum = 0;
+	for (i = 0; i < 4; i++) {
+		int x1, x2, y1, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		maxX = max(maxX, x2);
+		maxY = max(maxY, y2);
+		sum += ((x2 - x1) * (y2 - y1));
+		int c, d;
+		for (c = x1; c < x2; c++) {
+			for (d = y1; d < y2; d++) {
+				arr[c][d] += 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < maxX; i++) {
+		for (int j = 0; j < maxY; j++) {
+			if (arr[i][j] > 1) {
+				sum -= (arr[i][j] - 1);
+			}
+		}
+	}
+	cout << sum << endl;
+}
