ab040806be887336638baf2346e5678fea0f635e
1876070677 <fixerpremium@gmail.com>
2024-01-04 17:55:45 +0900

    ADD: BoJ 14719

---
+++ week_1/14719.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int H, W;
+	cin >> H >> W;
+	int arr[500];
+	for (int i = 0; i < W; i++) {
+		int input;
+		cin >> input;
+		arr[i] = input;
+	}
+
+	int sum = 0;
+
+	for (int i = 1; i < W - 1; i++) {
+		int maxL = arr[i], maxR = arr[i];
+		for (int j = 0; j < i; j++) {
+			maxL = max(maxL, arr[j]);
+		}
+		for (int j = i + 1; j < W; j++) {
+			maxR = max(maxR, arr[j]);
+		}
+		sum += (min(maxL, maxR) - arr[i]);
+	}
+	cout << sum << endl;
+}
