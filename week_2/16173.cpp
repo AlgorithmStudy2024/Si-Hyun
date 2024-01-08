57cbbc7159f73a542677d301d2811c92ed70a6ea
1876070677 <fixerpremium@gmail.com>
2024-01-08 22:54:08 +0900

    ADD: BoJ 16173

---
+++ week_2/16173.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+int map[3][3];
+int visit[3][3] = { 0, };
+bool check = false;
+
+void dfs(int x1, int x2) {
+	if (map[x1][x2] == -1) {
+		check = true;
+	}
+	else if (!visit[x1][x2]) {
+		visit[x1][x2] = 1;
+		if (x1 + map[x1][x2] <= N)
+			dfs(x1 + map[x1][x2], x2);
+		if (x2 + map[x1][x2] <= N)
+			dfs(x1, x2 + map[x1][x2]);
+	}
+}
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int temp;
+			cin >> temp;
+			map[i][j] = temp;
+		}
+	}
+	dfs(0, 0);
+	if (check) {
+		cout << "HaruHaru" << '\n';
+	}
+	else {
+		cout << "Hing" << '\n';
+	}
+}
